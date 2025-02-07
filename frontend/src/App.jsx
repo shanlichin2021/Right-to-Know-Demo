e6155d9a8862595480fdf35d4543c83f7a24f702
shanlichin2021 <shanlichin2021@fau.edu>
2025-02-07 17:00:21 -0500

    Created Authentification Context and Protected Route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,25 +7,31 @@ import { ChatProvider } from "./components/ChatContext";
 import FormPage from "./components/FormPage";
 import { ModelEndpointProvider } from "./components/ModelEndpointContext";
 import LandingPage from "./components/LandingPage";
+import AuthModal from "./components/AuthModal";
+import { AuthProvider } from "./components/AuthContext";
 
 const App = () => {
   return (
     <ChatProvider>
-      <ModelEndpointProvider>
-        <Router>
-          <div className="h-screen flex flex-col">
-            <TopBar />
-            <div className="flex-1">
-              <Routes>
-                <Route path="/" element={<LandingPage />} />
-                <Route path="/form" element={<FormPage />} />
-                <Route path="/about" element={<AboutPage />} />
-                <Route path="/chat" element={<ChatPage />} />
-              </Routes>
+      <AuthProvider>
+        <ModelEndpointProvider>
+          <Router>
+            {/* AuthModal overlays all pages until user is logged in */}
+            <AuthModal />
+            <div className="h-screen flex flex-col">
+              <TopBar />
+              <div className="flex-1">
+                <Routes>
+                  <Route path="/" element={<LandingPage />} />
+                  <Route path="/form" element={<FormPage />} />
+                  <Route path="/about" element={<AboutPage />} />
+                  <Route path="/chat" element={<ChatPage />} />
+                </Routes>
+              </div>
             </div>
-          </div>
-        </Router>
-      </ModelEndpointProvider>
+          </Router>
+        </ModelEndpointProvider>
+      </AuthProvider>
     </ChatProvider>
   );
 };
